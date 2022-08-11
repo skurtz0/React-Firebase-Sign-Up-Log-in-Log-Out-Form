f9bc2a593b6295a8069556b8ebeba44f7aff3777
SKURTZ <108894925+skurtz0@users.noreply.github.com>
2022-08-11 15:26:15 +0300

    Update firebase.js

--- src/firebase.js
+++ src/firebase.js
@@ -6,12 +6,12 @@ import { getAuth, createUserWithEmailAndPassword, signInWithEmailAndPassword, on
  from "firebase/auth";
 
 const firebaseConfig = {
-    apiKey: "AIzaSyDGKmjAwsr7oDdu6QVccky_LdxjcM6M9lE",
-    authDomain: "react-firebase-auth-a78e4.firebaseapp.com",
-    projectId: "react-firebase-auth-a78e4",
-    storageBucket: "react-firebase-auth-a78e4.appspot.com",
-    messagingSenderId: "900908429182",
-    appId: "1:900908429182:web:b9061af105da41473a0d2f"
+    apiKey: "",
+    authDomain: "",
+    projectId: "",
+    storageBucket: "",
+    messagingSenderId: "",
+    appId: ""
 };
 
 const app = initializeApp(firebaseConfig);
